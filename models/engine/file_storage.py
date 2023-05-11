72d79b02462d80be19bffce8c36b7ff550c8caff
ziontsion <tsionnew@gmail.com>
2023-05-11 05:37:28 -0700

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,21 +22,4 @@ class FileStorage:
         objcname = obj.__class__.__name__
         FileStorage.__objects["{}.{}".format(objcname, obj.id)] = obj
 
-    def save(self):
-        """Serialize __objects to the JSON file __file_path."""
-        objdict_1 = FileStorage.__objects
-        objdict = {obj: objdict_1[obj].to_dict() for obj in objdict_1.keys()}
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(objdict, f)
-
-    def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
-        try:
-            with open(FileStorage.__file_path) as f:
-                objdict = json.load(f)
-                for o in objdict.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
-        except FileNotFoundError:
-            return
+ 
